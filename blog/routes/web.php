dfd528c6222c1c2be1a99da992a9c5b87251b182
Ing Guillermo Malagon <guillomal373@gmail.com>
2024-04-14 19:34:49 -0500

    Routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -7,6 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route with optional variables with if 
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if($categoria){
+    return "Bienvenido al curso $curso, de la categoria $categoria";
+}else{
+    return "Bienvenido al curso $curso sin categoria";
+    }
+});
+
 //Route with variable
 Route::get('/cursos/{curso}', function ($curso) {
     return "Hola Guillo, escribiste el curso: $curso";
